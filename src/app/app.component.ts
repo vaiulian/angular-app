71bf29d042d42f3402cefa12deefe5c1ab900085
vaiulian <vasileaiulian@gmail.com>
2018-10-26 10:52:24 +0300

    Add OnInit interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-new-app';
+export class AppComponent implements OnInit{
+  title:string;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.title = "Hello, My app"
+  }
 }
