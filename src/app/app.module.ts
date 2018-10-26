48319614a47c860ce03811f47fb6f17c565955ec
vaiulian <vasileaiulian@gmail.com>
2018-10-26 12:37:08 +0300

    Add Filter-TextBox Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersComponent } from './customers/customers.component';
 import { CustomersListComponent } from './customers/customers-list/customers-list.component';
+import { FilterTextboxComponent } from './customers/customers-list/filter-textbox/filter-textbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomersComponent,
-    CustomersListComponent
+    CustomersListComponent,
+    FilterTextboxComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/customers/customers-list/filter-textbox/filter-textbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-textbox',
+  templateUrl: './filter-textbox.component.html',
+  styleUrls: ['./filter-textbox.component.scss']
+})
+export class FilterTextboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
